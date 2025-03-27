75287739400be88113da33a86f737a17fdf85ef5
SatyanarayanPrashar <satyanarayanprashar73@gmail.com>
2025-03-27 23:19:22 +0530

    resume_builder

---
+++ be/resume_builder/utils/main.py
@@ -0,0 +1,79 @@
+import base64
+import os
+from pathlib import Path
+import warnings
+import inquirer
+import requests
+import yaml
+import subprocess
+import sys
+from typing import Type
+
+from manager_facade import FacadeManager
+from resume import Resume
+from style_manager import StyleManager
+from resume_generator import ResumeGenerator
+
+# Define custom deprecation warnings
+class CustomDeprecationWarning(DeprecationWarning):
+    """Custom warning for deprecated features."""
+    pass
+
+class CustomPendingDeprecationWarning(PendingDeprecationWarning):
+    """Custom pending deprecation warning."""
+    pass
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=PendingDeprecationWarning)
+
+
+
+@staticmethod
+def validate_secrets(secrets_yaml_path: Path):
+    try:
+        with open(secrets_yaml_path, 'r') as stream:
+            secrets = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        raise Exception(f"Error reading secrets file {secrets_yaml_path}: {exc}")
+    except FileNotFoundError:
+        raise Exception(f"Secrets file not found: {secrets_yaml_path}")
+    mandatory_secrets = ['openai_api_key']
+    for secret in mandatory_secrets:
+        if secret not in secrets:
+            raise Exception(f"Missing secret in file {secrets_yaml_path}: {secret}")
+    if not secrets['openai_api_key']:
+        raise Exception(f"OpenAI API key cannot be empty in secrets file {secrets_yaml_path}.")
+    return secrets['openai_api_key']
+
+def printred(text):
+    RED = "\033[91m"
+    RESET = "\033[0m"
+    print(f"{RED}{text}{RESET}")
+
+def main():  
+    folder = "log"
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    
+    log_path = Path(folder).resolve()
+    secrets_yaml_path = Path(__file__).parent / "secrets.yaml"
+    resume_yaml_path = Path(__file__).parent / "plain_text_resume.yaml"
+    api_key = validate_secrets(secrets_yaml_path)
+    
+    with open(resume_yaml_path, "r") as file:
+        plain_text_resume = file.read()
+        resume_object = Resume(plain_text_resume)
+    
+    style_manager = StyleManager()
+    resume_generator = ResumeGenerator()
+    
+    manager = FacadeManager(api_key, style_manager, resume_generator, resume_object, log_path)
+
+    if os.path.exists("resume.pdf"):
+        os.remove("resume.pdf")
+    
+    with open("resume.pdf", "xb") as f:
+        f.write(base64.b64decode(manager.pdf_base64()))
+
+if __name__ == "__main__":
+    main()
---
+++ be/resume_builder/utils/style_manager.py
@@ -0,0 +1,57 @@
+import os
+from pathlib import Path
+from typing import Dict, List, Tuple
+
+class StyleManager:
+    
+    def __init__(self):
+        self.styles_directory: Path | None = None  # Explicitly define type
+    
+    def set_styles_directory(self, styles_directory: Path):
+        if not styles_directory.is_dir():
+            raise ValueError(f"Invalid directory: {styles_directory}")
+        self.styles_directory = styles_directory
+
+    def get_styles(self) -> Dict[str, Tuple[str, str]]:
+        styles_to_files = {}
+        if self.styles_directory is None:
+            print("Styles directory is not set.")
+            return styles_to_files
+
+        if not self.styles_directory.exists():
+            print(f"Directory {self.styles_directory} not found.")
+            return styles_to_files
+        
+        try:
+            for file_path in self.styles_directory.iterdir():
+                if file_path.is_file():
+                    with open(file_path, 'r', encoding='utf-8') as file:
+                        first_line = file.readline().strip()
+                        if first_line.startswith("/*") and first_line.endswith("*/"):
+                            content = first_line[2:-2].strip()
+                            if '$' in content:
+                                style_name, author_link = content.split('$', 1)
+                                styles_to_files[style_name.strip()] = (file_path.name, author_link.strip())
+        except (FileNotFoundError, PermissionError) as e:
+            print(f"Error accessing {self.styles_directory}: {e}")
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+        
+        return styles_to_files
+
+    def format_choices(self, styles_to_files: Dict[str, Tuple[str, str]]) -> List[str]:
+        return [f"{style_name} (style author -> {author_link})" for style_name, (_, author_link) in styles_to_files.items()]
+
+    def get_style_path(self, selected_style: str) -> Path | None:
+        if self.styles_directory is None:
+            print("Styles directory is not set.")
+            return None
+        
+        styles = self.get_styles()
+        # print("styles: ", styles)
+        if selected_style not in styles:
+            print(f"Style '{selected_style}' not found.")
+            return None
+        
+        file_name, _ = styles[selected_style]
+        return self.styles_directory / file_name
